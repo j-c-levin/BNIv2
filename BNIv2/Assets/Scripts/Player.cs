19a40159e22d7ae16609c7a67b78de0b4209f1be
joshua levin <joshua.c.levin@gmail.com>
2016-01-28 22:20:27 +0000

    Create game scene and player behaving as bni

---
+++ BNIv2/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D Rigidbody;
+    public float movementSpeed;
+    public float jumpSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_STANDALONE || UNITY_WEBGL
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            Rigidbody.AddForce(new Vector3(movementSpeed * Input.GetAxisRaw("Horizontal"), 0, 0));
+            Rigidbody.AddForce(new Vector3(0, jumpSpeed, 0));
+        }
+
+#elif UNITY_ANDROID
+         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (Input.GetTouch(0).position.x > (Screen.width / 2))
+                {
+                    Rigidbody.AddForce(new Vector3(androidMovementSpeed * 1, 0, 0));
+                    Rigidbody.AddForce(new Vector3(0, jumpSpeed, 0));
+                }
+                else
+                {
+                    Rigidbody.AddForce(new Vector3(androidMovementSpeed * -1, 0, 0));
+                    Rigidbody.AddForce(new Vector3(0, jumpSpeed, 0));
+                }
+            }
+#endif
+    }
+}
