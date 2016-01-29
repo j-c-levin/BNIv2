580150d6b1a1a18d9c7c8ca2c533309cb522d13b
joshua levin <joshua.c.levin@gmail.com>
2016-01-29 23:22:32 +0000

    update file structure, create block manager, block script and create a simple block spawning loop.

---
+++ BNIv2/Assets/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BlockManager : MonoBehaviour
+{
+
+    public GameObject[] blockGroups;
+
+    GameObject initializingBlock;
+    BlockScript initialisingBlockScript;
+
+    public int initialLoopCountLevel;
+    public int loopCountModifier;
+    int loopCountLevel;
+    int loopCount;
+
+    public float initialDropSpeed;
+    public float dropSpeedModifier;
+    float dropSpeed;
+
+    public float initialDelayDuration;
+    public float delayDurtionModifier;
+    float delayDuration;
+
+    // Use this for initialization
+    void Start()
+    {
+        dropSpeed = initialDropSpeed;
+        delayDuration = initialDelayDuration;
+        loopCountLevel = initialLoopCountLevel;
+        StartCoroutine("blockLoop");
+    }
+
+    IEnumerator blockLoop()
+    {
+        instantiateBlock(dropSpeed);
+        yield return new WaitForSeconds(delayDuration);
+        loop();
+        StartCoroutine("blockLoop");
+    }
+
+    void loop()
+    {
+        loopCount += 1;
+        loopCount %= loopCountLevel;
+        if (loopCount == 0)
+        {
+            loopCountLevel += loopCountModifier;
+            dropSpeed += dropSpeedModifier;
+            delayDuration -= delayDurtionModifier;
+            Debug.Log("new loop level: " + loopCountLevel);
+        }
+    }
+
+    void instantiateBlock(float speed)
+    {
+        initializingBlock = Instantiate(blockRandomiser(), transform.position, Quaternion.identity) as GameObject;
+        initialisingBlockScript = initializingBlock.GetComponent<BlockScript>();
+        initialisingBlockScript.dropSpeed = speed;
+        initialisingBlockScript.beginDrop();
+    }
+
+    GameObject blockRandomiser()
+    {
+        return blockGroups[Random.Range(0, blockGroups.Length)];
+    }
+}
---
+++ BNIv2/Assets/Assets/Scripts/BlockScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockScript : MonoBehaviour {
+
+    public float dropSpeed;
+
+    public void beginDrop()
+    {
+        StartCoroutine("drop");
+    }
+
+    IEnumerator drop()
+    {
+        yield return new WaitForEndOfFrame();
+        transform.Translate(new Vector3(0, -dropSpeed * Time.deltaTime, 0));
+        StartCoroutine("drop");
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
